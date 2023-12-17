6e8c3c0bcf49ffa1dd69a305583d2365782165fa
Uday <uday@gmail.com>
2023-12-17 16:20:36 +0530

    Added new Branch

--- src/Employee_Wage_Computation.java
+++ src/Employee_Wage_Computation.java
@@ -7,15 +7,18 @@ public class Employee_Wage_Computation
         int full_time = 1;
         int wage_per_hour=20;
         int daily_work;
+        int part_time=2;
         Random random=new Random();
-        int employee=random.nextInt(2);
+        int employee=random.nextInt(3);
         if (employee == full_time)
         {
             daily_work=8;
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Present Full Time");
 
-        } else
-        {
+        } else if (employee==part_time) {
+             daily_work=8;
+            System.out.println("Employee is present Part Time");
+        } else {
             daily_work=0;
             System.out.println("Employee is Absent");
         }
