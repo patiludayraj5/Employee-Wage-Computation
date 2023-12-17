e5d2218ef020c9a9b207e11ebee225446bc8329d
Uday <uday@gmail.com>
2023-12-17 17:08:40 +0530

    Added new Branch

--- src/Employee_Wage_Computation.java
+++ src/Employee_Wage_Computation.java
@@ -8,24 +8,35 @@ public class Employee_Wage_Computation
         int wage_per_hour=20;
         int daily_work;
         final int part_time=2;
+        int absent=0;
         Random random=new Random();
-        int employee=random.nextInt(3);
-        switch (employee) {
-            case full_time -> {
-                System.out.println("Employee is Present Full Time");
-                daily_work = 8;
+        int total_wage=0;
+        int wage=0;
+        for (int day=1;day<=20;day++) {
+            int employee = random.nextInt(3);
+            switch (employee) {
+                case full_time -> {
+                    System.out.println("Employee is Present Full Time");
+                    daily_work = 8;
+                    total_wage++;
+                }
+                case part_time -> {
+                    daily_work = 8;
+                    System.out.println("Employee is present Part Time");
+                    total_wage++;
+                }
+                default -> {
+                    daily_work = 0;
+                    System.out.println("Employee is Absent");
+                    absent++;
+                }
             }
-            case part_time -> {
-                daily_work = 8;
-                System.out.println("Employee is present Part Time");
-            }
-            default -> {
-                daily_work = 0;
-                System.out.println("Employee is Absent");
-            }
+            wage=daily_work * wage_per_hour;
+            System.out.printf("Employee Day %d Wage: %d\n",day, wage);
+            System.out.println();
         }
-
-        System.out.printf("\nEmployee Daily Wage: %d",daily_work*wage_per_hour);
-
+        System.out.println();
+        System.out.println("Employee is absent for "+absent+" Days");
+        System.out.println("Total month wage is : "+total_wage*wage);
     }
 }
