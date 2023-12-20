1108074e6bbdd71f5e730f7479874a1a289133e1
Uday <uday@gmail.com>
2023-12-20 11:06:36 +0530

    Added UC-6

--- src/Employee_Wage_Computation.java
+++ src/Employee_Wage_Computation.java
@@ -5,38 +5,40 @@ public class Employee_Wage_Computation
     public static void main(String []args)
     {
         final int full_time = 1;
-        int wage_per_hour=20;
-        int daily_work;
         final int part_time=2;
+        int wage_per_hour=20;
+        int Max_Working_Days=20;
+        int Max_Working_Hours=100;
+        //int daily_work;
         int absent=0;
-        Random random=new Random();
         int total_wage=0;
+        int working_Hour=0;
         int wage=0;
-        for (int day=1;day<=20;day++) {
+        Random random=new Random();
+        System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "WorkingHour", "Wage", "Total working hrs");
+        for (int day=1,total_working_hours=0;day<=Max_Working_Days && total_working_hours<Max_Working_Hours
+                ;day++,total_working_hours+=working_Hour) {
             int employee = random.nextInt(3);
             switch (employee) {
-                case full_time -> {
-                    System.out.println("Employee is Present Full Time");
-                    daily_work = 8;
+                case full_time, part_time -> {
+                  //  System.out.println("Employee is Present Full Time");
+                    working_Hour = 8;
                     total_wage++;
                 }
-                case part_time -> {
-                    daily_work = 8;
-                    System.out.println("Employee is present Part Time");
-                    total_wage++;
-                }
+                // System.out.println("Employee is present Part Time");
                 default -> {
-                    daily_work = 0;
-                    System.out.println("Employee is Absent");
+                    working_Hour = 0;
+                  //  System.out.println("Employee is Absent");
                     absent++;
                 }
             }
-            wage=daily_work * wage_per_hour;
-            System.out.printf("Employee Day %d Wage: %d\n",day, wage);
-            System.out.println();
+            wage=working_Hour * wage_per_hour;
+            total_wage+=wage;
+            System.out.printf("%5d       %5d      %5d      %5d\n", day, working_Hour, wage, total_working_hours + working_Hour);            System.out.println();
         }
         System.out.println();
-        System.out.println("Employee is absent for "+absent+" Days");
-        System.out.println("Total month wage is : "+total_wage*wage);
+        System.out.println("Total wage for a month is " + total_wage);
+       // System.out.println("Employee is absent for "+absent+" Days");
+        //System.out.println("Total month wage is : "+total_wage*wage);
     }
 }
